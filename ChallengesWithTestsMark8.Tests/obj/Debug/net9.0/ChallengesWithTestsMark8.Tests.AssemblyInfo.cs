392c7460a1b6ae4b89adf7562b1dd8cb81ff1be4
TH8359 <hogue73@embarqmail.com>
2025-10-08 20:03:51 -0400

    completed the second set of challenges

--- ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab17fdb77f100a505334212e709c332a54d3ce14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5392813a1a5dcca373e797df2b6a8a778aa2a660")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -7,52 +7,226 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+
+            string alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+            string alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            foreach (char letter in alphabetLower)
+            {
+                if (letter == c)
+                {
+                    return true;
+                }
+            }
+
+            foreach (char letter in alphabetUpper)
+            {
+                if (char.ToUpper(c) == letter)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else if (num == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+
+            double total = 0;
+            int counter = 0;
+            double lowestNumber = 0;
+            double highestNumber = 0;
+            
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            int numberOfItems = 0;
+            
+            foreach (double number in numbers)
+            {
+                numberOfItems++;
+            }
+            
+            double[] itemList = new double[numberOfItems];
+
+            foreach (double number in numbers)
+            {
+                
+                itemList[counter] = number;
+                counter++;
+            }
+
+            if (itemList.Length == 1)
+            {
+                total = itemList[0] + itemList[0];
+                return total;
+            }
+
+            for (int counterTwo = 0; counterTwo < itemList.Length; counterTwo++)
+            {
+                if (counterTwo == 0)
+                {
+                    lowestNumber = itemList[0];
+                    highestNumber = itemList[0];
+                }
+
+                if (itemList[counterTwo] < lowestNumber)
+                {
+                    lowestNumber = itemList[counterTwo];
+                }
+
+                if (itemList[counterTwo] > highestNumber)
+                {
+                    highestNumber = itemList[counterTwo];
+                }
+            }
+            
+            total =  lowestNumber + highestNumber;
+            
+
+            
+
+            return total;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else if (str2.Length < str1.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+            
+            int total = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    total += number;
+                }
+            }
+            
+            return total;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return false;
+            }
+            
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            else if (sum == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long validNumbers = 0;
+            long startingNumber = number - 1;
+            while (startingNumber > 0)
+            {
+                if (startingNumber % 2 != 0 && startingNumber != 0)
+                {
+                    validNumbers++;
+                }
+                startingNumber--;
+            }
+            return validNumbers;
         }
     }
 }
